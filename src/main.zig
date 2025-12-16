a3292bf84f29fb8915ae9e16b863759289a2b0e1
Cezar <fntblk@gmail.com>
2025-12-16 11:04:36 -0300

    starting the 32-bit x86 codegen

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,28 @@
 const std = @import("std");
+const IR = @import("ir.zig").IR;
+const x86 = @import("x86.zig");
 
 pub fn main() !void {
-    std.debug.print("hello world!\n", .{});
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const input = "+++.";
+    var reader = std.Io.Reader.fixed(input);
+
+    const ir_code = try IR.lex(&reader, allocator);
+    defer allocator.free(ir_code);
+
+    const x86_code = try x86.codegen(ir_code, allocator);
+    defer allocator.free(x86_code);
+
+    std.debug.print("{s}\n", .{x86_code});
 }
 
 test "test index" {
     comptime {
         _ = @import("ir.zig");
+        _ = @import("x86.zig");
     }
     std.testing.refAllDecls(@This());
 }
---
+++ src/x86.zig
@@ -0,0 +1,74 @@
+//! ECX register is to always hold the current cell pointer
+
+const std = @import("std");
+const IR = @import("ir.zig").IR;
+
+const prolog =
+    \\;;; Compile with:
+    \\;;;  nasm -f elf32 <source>.asm -o <object>.o
+    \\;;; Link with:
+    \\;;;  gcc -no-pie -m32 <object>.o
+    \\
+    \\global main
+    \\extern printf
+    \\
+    \\section .note.GNU-stack noalloc noexec nowrite progbits
+    \\
+    \\section .data
+    \\    good_output_fmt db "%c", 0x0A, 0x00
+    \\    bad_output_fmt  db "Bad Char: '%d'", 0x0A, 0x00
+    \\
+    \\section .bss
+    \\    cells resd 30000
+    \\
+    \\section .text
+    \\
+    \\print_char:
+    \\push ecx ; save ECX, printf may mutate it
+    \\mov eax, [cells + ecx*4]
+    \\cmp eax, 10 ; (cells[ptr] == '\n') -> good char exception
+    \\je PRINT_GOOD_CHAR
+    \\cmp eax, 32 ; (cells[ptr] < ' ') -> bad char
+    \\jl PRINT_BAD_CHAR
+    \\cmp eax, 126 ; (cells[ptr] > '~') -> bad char
+    \\jg PRINT_BAD_CHAR
+    \\PRINT_GOOD_CHAR:
+    \\push eax
+    \\push good_output_fmt
+    \\jmp PRINT_EXIT
+    \\PRINT_BAD_CHAR:
+    \\push eax
+    \\push bad_output_fmt
+    \\jmp PRINT_EXIT
+    \\PRINT_EXIT:
+    \\call printf
+    \\add esp, 8
+    \\pop ecx  ; get back saved ECX value
+    \\ret
+    \\
+    \\main:
+    \\mov ecx, 0
+    \\mov DWORD [cells + ecx*4], 65
+    \\call print_char
+    \\mov ecx, 1
+    \\mov DWORD [cells + ecx*4], 66
+    \\call print_char
+    \\
+;
+
+const epilog =
+    \\
+    \\xor eax,eax
+    \\ret
+;
+
+pub fn codegen(ir_array: []IR, allocator: std.mem.Allocator) ![]u8 {
+    var assembly_buffer: std.ArrayList(u8) = .empty;
+    defer assembly_buffer.deinit(allocator);
+
+    try assembly_buffer.appendSlice(allocator, prolog);
+    _ = ir_array;
+    try assembly_buffer.appendSlice(allocator, epilog);
+
+    return try assembly_buffer.toOwnedSlice(allocator);
+}
