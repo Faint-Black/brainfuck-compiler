f427e17a7d3acf7656987a45082784b80e271b96
Cezar <fntblk@gmail.com>
2025-12-16 15:51:03 -0300

    command line argument parser

---
+++ src/clap.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+
+pub const CLAP = struct {
+    input_filepath: ?[]const u8 = null,
+    print_help: bool = false,
+    print_version: bool = false,
+
+    pub fn deinit(self: CLAP, allocator: std.mem.Allocator) void {
+        if (self.input_filepath) |mem| allocator.free(mem);
+    }
+
+    pub fn parseArgs(allocator: std.mem.Allocator) !CLAP {
+        var result = CLAP{};
+        errdefer result.deinit(allocator);
+        var iterator = try std.process.ArgIterator.initWithAllocator(allocator);
+        defer iterator.deinit();
+        var counter: usize = 0;
+        while (iterator.next()) |arg| : (counter += 1) {
+            // skip binary path
+            if (counter == 0) continue;
+
+            if (std.mem.eql(u8, arg, "-h")) {
+                result.print_help = true;
+            } else if (std.mem.eql(u8, arg, "--help")) {
+                result.print_help = true;
+            } else if (std.mem.eql(u8, arg, "--version")) {
+                result.print_version = true;
+            } else {
+                if (result.input_filepath != null) {
+                    return error.OnlyNeedOneFilepath;
+                }
+                result.input_filepath = try allocator.dupe(u8, arg);
+            }
+        }
+        if (!result.anyInfoFlagIsActive() and result.input_filepath == null) {
+            return error.NoSourceProvided;
+        }
+        return result;
+    }
+
+    /// returns true if either 'help' or 'version' flags are active
+    pub fn anyInfoFlagIsActive(self: CLAP) bool {
+        return (self.print_help or self.print_version);
+    }
+
+    pub const help_string =
+        \\Brainfuck Compiler, a brainfuck compiler...
+        \\
+        \\SYNOPSIS
+        \\       brainfuck-compiler [source-file]
+        \\
+        \\USAGE
+        \\       brainfuck-compiler brainfuck-source.txt
+        \\
+    ;
+
+    pub const version_string =
+        \\Brainfuck Compiler
+        \\version 1.0.0
+        \\
+    ;
+};
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const CLAP = @import("clap.zig").CLAP;
 const IR = @import("ir.zig").IR;
 const x86 = @import("x86.zig");
 
@@ -7,10 +8,20 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const input = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-    var reader = std.Io.Reader.fixed(input);
+    const clap = try CLAP.parseArgs(allocator);
+    defer clap.deinit(allocator);
+    if (clap.anyInfoFlagIsActive()) {
+        if (clap.print_help) std.debug.print("{s}", .{CLAP.help_string});
+        if (clap.print_version) std.debug.print("{s}", .{CLAP.version_string});
+        return;
+    }
 
-    const ir_code = try IR.lex(&reader, allocator);
+    var reader_buffer: [4096]u8 = undefined;
+    var file = try std.fs.cwd().openFile(clap.input_filepath.?, .{});
+    var file_reader = file.reader(&reader_buffer);
+    const reader = &file_reader.interface;
+
+    const ir_code = try IR.lex(reader, allocator);
     defer allocator.free(ir_code);
 
     const x86_code = try x86.codegen(ir_code, allocator);
